135fbfbb77034285a711367935bb47eaeabb1e8e
tranhunter84 <tranhunter84@gmail.com>
2024-11-03 20:14:01 -0800

    L16 dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// todo
+import "./style.css";
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const button = document.createElement("button");
+
+button.textContent = "Click me!";
+
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+
+app.append(button);
