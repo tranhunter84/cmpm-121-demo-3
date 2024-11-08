f8e0c96609da08c8ba370bd6738ea0f029be3ca3
tranhunter84 <tranhunter84@gmail.com>
2024-11-08 13:38:16 -0800

    Adding marker to mark player location

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import leaflet from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./style.css";
 import "./leafletWorkaround.ts";
-// import luck from "./luck.ts";
 
 // CORE VARIABLES
-//const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
-const playerLocation = [36.98949379578401, -122.06277128548504];
+const playerLocation: [number, number] = [
+  36.98949379578401,
+  -122.06277128548504,
+];
 const app: HTMLDivElement = document.querySelector("#app")!;
 const appTitle = "Geocoin Carrier";
 const header = document.createElement("h1");
@@ -19,6 +20,8 @@ function initializeMap(mapContainer: HTMLElement) {
     center: playerLocation,
     zoom: 18,
   });
+
+  // Add a tile layer to the map
   leaflet
     .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
@@ -26,6 +29,12 @@ function initializeMap(mapContainer: HTMLElement) {
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     })
     .addTo(map);
+
+  leaflet
+    .marker(playerLocation)
+    .addTo(map)
+    .bindPopup("You are here!")
+    .openPopup();
 }
 
 // App Title & Header
@@ -35,7 +44,7 @@ header.innerHTML = appTitle;
 // Adding Basic Button
 button.textContent = "Click me!";
 button.addEventListener("click", () => {
-  alert("you clicked the button!");
+  alert("You clicked the button!");
 });
 button.style.backgroundColor = "white";
 button.style.color = "black";
